53b1cc870d00fc543c21780b6f8fc7de64ea956b
TeamDman <TeamDman9201@gmail.com>
2022-10-14 01:25:37 -0400

    actual default board working

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 from __future__ import annotations
 from dataclasses import dataclass
-import enum
+from enum import unique, auto, Enum
 
 from torch import Tensor
 import torch
 
-@enum.unique
-class Piece(enum.Enum):
+@unique
+class Piece(Enum):
     AIR = 0
-    PAWN = 1
-    ROOK = 2
-    KNIGHT = 3
-    BISHOP = 4
-    QUEEN = 5
-    KING = 6
+    BLACK_PAWN = auto()
+    BLACK_ROOK = auto()
+    BLACK_KNIGHT = auto()
+    BLACK_BISHOP = auto()
+    BLACK_QUEEN = auto()
+    BLACK_KING = auto()
+    WHITE_PAWN = auto()
+    WHITE_ROOK = auto()
+    WHITE_KNIGHT = auto()
+    WHITE_BISHOP = auto()
+    WHITE_QUEEN = auto()
+    WHITE_KING = auto()
 
 
 @dataclass(frozen=True)
@@ -29,15 +35,24 @@ class State:
     @staticmethod
     def get_starting_state() -> State:
         board = torch.zeros(8,8,len(Piece))
-        board[[1,6],:,Piece.PAWN.value] = 1
-        board[[0,0,7,7],[0,7,0,7], Piece.ROOK.value] = 1 
-        board[[0,0,7,7],[1,6,1,6], Piece.KNIGHT.value] = 1 
-        board[[0,0,7,7],[2,5,2,5], Piece.BISHOP.value] = 1 
-        board[[0,7],[3,3], Piece.QUEEN.value] = 1
-        board[[0,7],[4,4], Piece.KING.value] = 1
+        board[1,:,Piece.BLACK_PAWN.value] = 1
+        board[6,:,Piece.WHITE_PAWN.value] = 1
+        board[[0,0],[0,7], Piece.BLACK_ROOK.value] = 1 
+        board[[7,7],[0,7], Piece.WHITE_ROOK.value] = 1 
+        board[[0,0],[1,6], Piece.BLACK_KNIGHT.value] = 1 
+        board[[7,7],[1,6], Piece.WHITE_KNIGHT.value] = 1 
+        board[[0,0],[2,5], Piece.BLACK_BISHOP.value] = 1 
+        board[[7,7],[2,5], Piece.WHITE_BISHOP.value] = 1 
+        board[0,3, Piece.BLACK_QUEEN.value] = 1
+        board[7,3, Piece.WHITE_QUEEN.value] = 1
+        board[0,4, Piece.BLACK_KING.value] = 1
+        board[7,4, Piece.WHITE_KING.value] = 1
         board[2:6,:,Piece.AIR.value] = 1
         # board[board==torch.zeros(7), Piece.AIR.value] = 12
         return State(board=board)
+
+    def is_checkmate(self) -> bool:
+        pass
 
 class Game:
     state: State
